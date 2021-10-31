06ee7cb989d5a498c3aa03214efe8e417ae33759
ochanteau <chanteau.o@gmail.com>
2021-10-31 19:37:53 +0100

    redaction des focntiosn de chaque etape de la validation panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -5,10 +5,10 @@ console.log(cart);
 
 // ciblage prix total et quantité total
 
-let  selectTotalQuantity = document.getElementById("totalQuantity");
-let  selectTotalPrice = document.getElementById("totalPrice");
+const  selectTotalQuantity = document.getElementById("totalQuantity");
+const  selectTotalPrice = document.getElementById("totalPrice");
 
-// vérification que le panier n 'est pas vide
+// vérification que le panier n 'est pas vide ou Null
 
 function updateCart() {
     if (cart == null || cart.length == 0) {
@@ -19,7 +19,7 @@ function updateCart() {
 
   }
 
-// Fonction qui affiche les différents éléménts du panier sur la page
+// Fonction qui lance l'affichage des différents éléménts du panier sur la page ainsi que la quantité et le prix total
 
 function displayCart(array) {
     let totalQuantity = 0;
@@ -96,8 +96,7 @@ cartList.addEventListener("change", event => {
   const cartItem = event.target.closest(".cart__item");
   if (event.target.value == 0) { removeItem(cart,cartItem)}
   else {quantityModification (cart,cartItem,event)}
-  document.querySelector("#cart__items").innerHTML ="";
-  // displayCart(cart);  
+  document.querySelector("#cart__items").innerHTML =""; 
   updateCart();
   localStorage.clear();
   localStorage.setItem("panier", JSON.stringify(cart));
@@ -108,7 +107,7 @@ function quantityModification (array,element,event) {
     console.log(findIndex);
     array[findIndex].quantity = Number(event.target.value);
     console.log(event.target.value);
-    // document.querySelector("#cart__items").innerHTML ="";
+   
    
   }
   
@@ -134,3 +133,118 @@ function quantityModification (array,element,event) {
 
   updateCart();
     // localStorage.clear();
+
+
+
+
+
+// valdiation commande
+
+const selectForm = document.querySelector(".cart__order__form");
+const selectFirstName = document.getElementById("firstName");
+const selectLastName = document.getElementById("lastName");
+const selectAddress = document.getElementById("address");
+const selectCity = document.getElementById("city");
+const selectEmail = document.getElementById("email");
+
+class Contact {
+  constructor(firstname, lastname,address,city,email) {
+  this.firstName = firstname;
+  this.lastName = lastname;
+  this.address = address;
+  this.city = city;
+  this.email = email;
+  }
+  }
+
+
+function checkForm () {
+  let counter = 0;
+  if (/\d/.test(selectFirstName.value)) {
+    document.getElementById("firstNameErrorMsg").textContent = "Merci d'inscrire votre prénom en lettre" ;
+  } else { counter += 1}
+  if (/\d/.test(selectLastName.value)) {
+    document.getElementById("lastNameErrorMsg").textContent = "Merci d'inscrire votre nom en lettre" ;
+  } else { counter += 1}
+  if (/\d/.test(selectAddress.value)) {
+    document.getElementById("addressErrorMsg").textContent = "Merci d'inscrire votre adresse" ;
+  } else { counter += 1}
+  if (/\d/.test(selectCity.value)) {
+    document.getElementById("cityErrorMsg").textContent = "Merci d'inscrire votre ville en lettre" ;
+  } else { counter += 1}
+  if (/\d/.test(selectEmail.value)) {
+    document.getElementById("emailErrorMsg").textContent = "Merci de renseigner un email valide" ;
+  } else { counter += 1}
+  return counter
+
+}
+
+selectForm.addEventListener ("submit", (event) => {
+  const check = checkForm();
+  if (check != 5 ){ event.preventDefault}
+  else{
+    const contact = new Contact (selectFirstName.value,selectLastName.value,selectAddress.value,selectCity.value,selectEmail.value);
+    console.log (contact);
+    // const idArray =[];
+    const idArray = setIdArray (cart);
+    // setIdArray (cart,idArray);
+    console.log (idArray);
+
+  }
+})
+
+
+function setIdArray (array) {
+ 
+  array.map(item=>item.id);
+  return array.map(item=>item.id);
+}
+
+
+const contact = new Contact ("dzdzd","dzdzd","dzdzd","23 adededede dedede", "deded@dezdz.fr");
+
+const products = ["415b7cacb65d43b2b5c1ff70f3393ad1" ]
+
+fetch("http://localhost:3000/api/products/order", {
+	method: "POST",
+	headers: { 
+'Accept': 'application/json', 
+'Content-Type': 'application/json' 
+},
+	// body: JSON.stringify({contact:test,products: testid})
+  // body :  JSON.stringify({"contact" : test, "products": testid})
+  // body : { "contact" : test, "products": testid}
+  body : JSON.stringify({contact,products})
+})
+.then (async response => {
+  try{
+    const id = await response.json();
+    console.log(id)
+   
+  } catch (e){
+    console.log(e);
+  }
+})
+
+
+/**
+ *
+ * Expects request to contain:
+ * contact: {
+ *   firstName: string,
+ *   lastName: string,
+ *   address: string,
+ *   city: string,
+ *   email: string
+ * }
+ * products: [string] <-- array of product _id
+ *
+ */
+
+
+// console.log(/[a-zA-Z]/.test("Piere-eZE"));
+// console.log(/\d/.test("PiereeZE23"))
+// console.log((/[a-zA-Z]/.test("Piere-eZE"))& (/\D/.test("23")))
+// if (/[a-zA-Z]/.test("Piere-eZE")& /\D/.test("PiereeZE23")) {
+//   console.log("ok")
+// }
