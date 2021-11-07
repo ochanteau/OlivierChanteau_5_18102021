6282b5a3a7aa8595fd0a7606fd9d7af905461c63
ochanteau <chanteau.o@gmail.com>
2021-11-07 10:36:42 +0100

    refact page produit , indentation et commentaires

--- front/js/product.js
+++ front/js/product.js
@@ -8,22 +8,30 @@ var url = new URL (currentPage);
 const id = url.searchParams.get("id");
 
 
-// fonction pour afficher les caractéristiques du produit sur la page produit
+
+/*  
+*   fonction displayProduct () qui effectue une requete fetch vers API 
+*   pour obtenir un produit spécifique en utilisant la variable id et 
+*   appelle la fonction displayResult avec le resulat
+*/
 function displayProduct  () {
     fetch(`http://localhost:3000/api/products/${id}`)
     .then (async response => {
       try{
         const product = await response.json();
-     
         displayResult(product);
-      } catch (e){
+      }
+      catch (e){
         console.log(e);
       }
     })
   }
   
-// fonction pour inserer les caractéristiques du produit du tableau de produits , prend en parametre un objet
-
+/*  
+*  fonction displayResult(product) pour inserer les différents éléments du produit dans le DOM
+*  @param : {object} poduct , un objet pour récupérer les cles suivantes:
+* (poduct.imageUrl,poduct.altTxt,poduct.name,poduct.price,poduct.description, poduct.colors (array of colors))
+*/
 function displayResult(product) {
     const img = document.createElement("img");
     img.setAttribute("src", `${product.imageUrl}`)
@@ -42,19 +50,19 @@ function displayResult(product) {
     product.colors.forEach(color=> {
         const option = document.createElement("option");
         option.setAttribute("value", `${color}`);
-        option.textContent = `${color}`
+        option.textContent = `${color}`;
         document.getElementById("colors").append(option);
-      });    
+    });    
   }
 
 
-// 
+    
+/*  
+*  Classe CartItem , creation d'un élément standart du panier 
+*  constructeur : { (id: string), (color: string), (quantity: number)}
+* 
+*/
 
-// const cart = [{id: '107fb5b75607497b96722bda5b504926', color: 'Blue', quantity: 3},
-// {id: '107fb5b75607497b96722bda5b504926', color: 'White', quantity: 3}];
-// let cart = [];
-// let cart = [JSON.parse(localStorage.getItem("panier"))];
-// console.log(cart);
 
 class cartItem {
     constructor(id, color, quantity) {
@@ -62,46 +70,60 @@ class cartItem {
     this.color = color;
     this.quantity = quantity;
     }
-    }
+}
 
 
+
+// ciblage du boutton ajouter au panier
  const addToCart= document.getElementById("addToCart");
- let cart = JSON.parse(localStorage.getItem("panier"));
- console.log(cart);
 
+
+ /*  
+*  
+*  creation d'une variable 
+*  qui recupére l'array associé à la  clé "panier" du local storage
+*  au format objet javascript
+*/
+
+ let cart = JSON.parse(localStorage.getItem("panier"));
+
+/*
+* addToCart.addEventListener
+* Mise en place d'un EventListner sur le boutton ajouter au panier
+* verifie que l'utilisateur a bien saisi une quantité et une couleur et
+* Recupere la quantité et la couleur choisie par l'utilisateur
+* créé une instance de CartItem avec les valeurs recupérées et l'id du produit
+* Si la varibale cart est null on assigne l'objet crée à cart sinon on appelle
+* la fonction verification
+* on termine par effacer le localstorage et lui assigner la clé "panier" avec 
+* pour valeur la variable cart au format JSON 
+*/
 
  addToCart.addEventListener("click", ()=> {
     const itemQuantity=  Number(document.getElementById("quantity").value);
     const selectColor = document.getElementById("colors").value;
     const  product = new cartItem (id,selectColor,itemQuantity);
-    console.log(product);
     
-    
-    
-
-
     if ( itemQuantity !== 0 && selectColor !=="") {
-      if (cart == null) { cart = [product]}
-      else {verification (cart,product) }
-  
-      console.log(cart);
-      localStorage.clear();
-     
-      localStorage.setItem("panier", JSON.stringify(cart));
+        if (cart == null) { cart = [product]}
+        else {verification (cart,product) }
+        localStorage.clear();
+        localStorage.setItem("panier", JSON.stringify(cart));
     } 
-  
-
-    // if (cart[0] == null) { cart[0] = product}
-    // else {verification (cart,product) }
-
-    // console.log(cart);
-    // localStorage.clear();
-   
-    // localStorage.setItem("panier", JSON.stringify(cart));
-    
-      
 })
 
+
+/*  
+*  function verification (array,object)
+*  fonction qui verifie si la clé id d'un objet produit et sa clé couleur sont deja presents
+*  dans un array 
+   Si ces clés sont déja présentes on modifie la clé "quantity" avec l'index retourné par la 
+   méthode findIndex
+   Si elles ne sont pas présentes on ajoute  l'objet au tableau avec la methode push.
+*  @param : {array} tableau sur lequel on va itérer  
+*  @param : {object} objet sur lequel on va recuperer {object.id} et {object.color}
+*  pour vérifier leur presence dans le tableau
+*/
 
 function verification (array,object) {
   const verification = array.findIndex(x=> x.id === object.id && x.color === object.color );
@@ -114,13 +136,6 @@ function verification (array,object) {
 
 
 
-
-// localStorage.setItem("panier", JSON.stringify(cart));
-console.log(localStorage);
-console.log(JSON.parse(localStorage.getItem("panier")));
-// localStorage.clear();
-
-// console.log(JSON.parse(localStorage.getItem("panier")));
  displayProduct  ();
 
 
